40ea5936f11e4d64fed893d077fc23176261bb2a
alber <alber@ADAL-HG>
2022-05-22 22:53:47 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace howto_draw_normal_distribution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.picGraph2 = new System.Windows.Forms.PictureBox();
+            this.picGraph = new System.Windows.Forms.PictureBox();
+            this.lmovie = new System.Windows.Forms.Label();
+            this.lserie = new System.Windows.Forms.Label();
+            this.tbrandom = new System.Windows.Forms.TextBox();
+            this.sm = new System.Windows.Forms.Label();
+            this.ns = new System.Windows.Forms.Label();
+            this.st = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(12, 12);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // picGraph2
+            // 
+            this.picGraph2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGraph2.BackColor = System.Drawing.Color.White;
+            this.picGraph2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picGraph2.Location = new System.Drawing.Point(258, 75);
+            this.picGraph2.Name = "picGraph2";
+            this.picGraph2.Size = new System.Drawing.Size(257, 271);
+            this.picGraph2.TabIndex = 8;
+            this.picGraph2.TabStop = false;
+            // 
+            // picGraph
+            // 
+            this.picGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGraph.BackColor = System.Drawing.Color.White;
+            this.picGraph.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picGraph.Location = new System.Drawing.Point(2, 75);
+            this.picGraph.Name = "picGraph";
+            this.picGraph.Size = new System.Drawing.Size(257, 271);
+            this.picGraph.TabIndex = 7;
+            this.picGraph.TabStop = false;
+            // 
+            // lmovie
+            // 
+            this.lmovie.AutoSize = true;
+            this.lmovie.Location = new System.Drawing.Point(23, 59);
+            this.lmovie.Name = "lmovie";
+            this.lmovie.Size = new System.Drawing.Size(36, 13);
+            this.lmovie.TabIndex = 9;
+            this.lmovie.Text = "Movie";
+            // 
+            // lserie
+            // 
+            this.lserie.AutoSize = true;
+            this.lserie.Location = new System.Drawing.Point(291, 59);
+            this.lserie.Name = "lserie";
+            this.lserie.Size = new System.Drawing.Size(31, 13);
+            this.lserie.TabIndex = 10;
+            this.lserie.Text = "Serie";
+            // 
+            // tbrandom
+            // 
+            this.tbrandom.Location = new System.Drawing.Point(93, 15);
+            this.tbrandom.Name = "tbrandom";
+            this.tbrandom.Size = new System.Drawing.Size(124, 20);
+            this.tbrandom.TabIndex = 11;
+            // 
+            // sm
+            // 
+            this.sm.AutoSize = true;
+            this.sm.Location = new System.Drawing.Point(223, 21);
+            this.sm.Name = "sm";
+            this.sm.Size = new System.Drawing.Size(107, 13);
+            this.sm.TabIndex = 12;
+            this.sm.Text = "Serie duracion Mayor";
+            // 
+            // ns
+            // 
+            this.ns.AutoSize = true;
+            this.ns.Location = new System.Drawing.Point(377, 46);
+            this.ns.Name = "ns";
+            this.ns.Size = new System.Drawing.Size(89, 13);
+            this.ns.TabIndex = 13;
+            this.ns.Text = "Numero de series";
+            // 
+            // st
+            // 
+            this.st.AutoSize = true;
+            this.st.Location = new System.Drawing.Point(363, 22);
+            this.st.Name = "st";
+            this.st.Size = new System.Drawing.Size(103, 13);
+            this.st.TabIndex = 14;
+            this.st.Text = "Series duracion total";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(472, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnDraw;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.st);
+            this.Controls.Add(this.ns);
+            this.Controls.Add(this.sm);
+            this.Controls.Add(this.tbrandom);
+            this.Controls.Add(this.lserie);
+            this.Controls.Add(this.lmovie);
+            this.Controls.Add(this.picGraph2);
+            this.Controls.Add(this.picGraph);
+            this.Controls.Add(this.btnDraw);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "howto_draw_normal_distribution";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.PictureBox picGraph2;
+        private System.Windows.Forms.PictureBox picGraph;
+        private System.Windows.Forms.Label lmovie;
+        private System.Windows.Forms.Label lserie;
+        private System.Windows.Forms.TextBox tbrandom;
+        private System.Windows.Forms.Label sm;
+        private System.Windows.Forms.Label ns;
+        private System.Windows.Forms.Label st;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace howto_draw_normal_distribution
+{
+    public partial class Form1 : Form
+    {
+        Random ras = new Random(1);
+        int[] results = new int[2];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            results = new int[tam];
+            reset();
+            // vlcControl1.Play(new Uri(@"E:\series\english\Family Comedy\The Big Bang Theory\The Big Bang Theory Season 1\The Big Bang Theory S01E01.mkv"));
+        }
+        public double calcula_gauss()
+        {
+            int iterations = 3,
+            total = 0;
+            for (int i = 0; i < iterations; i += 1)
+            {
+                total += ras.Next(0, tam);
+            }
+            double a = (total / iterations);
+            int result = int.Parse(Math.Floor(a) + "");
+            if (results[result] < 3)
+                results[result] += 1;
+            return result;
+        }
+        public void calcula_precent(float[] percent_)//arreglo ordenado de mayor a menor, contiene el porcentaje
+        {
+            double rand = rass.NextDouble();
+            for (int i = 0; i < percent_.Length; i += 1)
+            {
+                if (rand < percent_[i])
+                {
+                    results[i] += 1;
+                    return;
+                    //return percent_[i];
+                }
+                rand -= percent_[i];
+            }
+            //return percent_[percent_.Length-1];
+        }
+        public double calcula_()
+        {
+            int iterations = 3,
+            total = 0;
+            for (int i = 0; i < iterations; i += 1)
+            {
+                total += ras.Next(0, tam);
+            }
+            double a = (total / iterations);
+            int result = int.Parse(Math.Floor(a) + "");
+            if (results[result] < 3)
+                results[result] += 1;
+            return result;
+        }
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            se();
+            draww();
+        }
+        public void draw()
+        {
+            Bitmap bm = new Bitmap(picGraph.ClientSize.Width, picGraph.ClientSize.Height);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+                using (Pen pen = new Pen(Color.Red, 0))
+                {
+                    using (Font font = new Font("Arial", 8))
+                    {
+                        pen.Color = Color.Black;
+                        var w = this.Width / tam;
+                        for (var i = 0; i < tam; i += 1)
+                        {
+                            var h = results[i] * 10;
+                            if (h != 0)
+                                gr.DrawRectangle(pen, i * 7, 0, 2, float.Parse(h + ""));
+                        }
+                    } // Font
+                } // Pen
+
+                picGraph.Image = bm;
+            }
+        }
+        // The normal distribution function.
+        private float F(float x, float one_over_2pi, float mean, float stddev, float var)
+        {
+            return (float)(one_over_2pi * Math.Exp(-(x - mean) * (x - mean) / (2 * var)));
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //calcula_gauss();
+            //draw();
+            se();
+            draww();
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //calcula_gauss();
+            //draw();
+        }
+
+
+        int tam = 7;
+
+        float[] results_chance = new float[] { 0.23f, 0.57f, 0.51f, 0.33f, 0.80f, 0.14f, 0.70f };
+        Random rass = new Random();
+        List<List<int>> series = new List<List<int>>();
+        float duracion = 0;
+        int limit = 2;//del arlgoritmo recursivo
+        int vc = 8;//numero de veces que se puede repetir una serie antes de ser eleiminada o marcada como reproducida //si ya fue completamente reproducida
+        float id_s = 0;
+        public void se()
+        {
+            lserie.Text = "Serie";//
+            double rand = rass.NextDouble();
+            if (rand < 0.021573)
+            {
+                results[0] += 1;//
+                lserie.Text = "Serie - pelicula";//
+            }
+            else
+                calcular_siguiente_serie();
+        }
+        public void calcular_siguiente_serie()
+        {
+            if (series.Count == 0)
+                reset();
+            float duracion_total = 0;
+            float duracion_mayor = 0;
+
+            foreach (List<int> ls in series)
+            {
+                duracion_total += ls[1];
+                if (ls[1] > duracion_mayor)
+                    duracion_mayor = ls[1];
+            }
+            sm.Text = "Mayor duracion S " + duracion_mayor;//
+            st.Text = "Total duracion: " + duracion_total;//
+            ns.Text = "N: " + series.Count;//
+            //primera busqueda con filtro de 2 para que exista dispercion
+            double rand = rass.NextDouble();
+            tbrandom.Text = rand + "";//
+            for (var i = 0; i < series.Count; i += 1)
+            {
+                List<int> ls = series[i];
+                if (ls[2] < limit)
+                {
+                    float p = (ls[1] / (duracion_mayor + (duracion_total / series.Count)));
+                    if (rand < p)
+                    {
+                        lserie.Text = "Serie - Se calculo otra " + ls[0];//
+                        id_s = ls[0];
+                        duracion = ls[1];
+                        ls[2] += 1;
+                        int b = (int)Math.Floor(10-(duracion_mayor_absoluto / ls[1]));
+                        label1.Text = b + " - " + ls[2];
+                        if (ls[2] > b) {
+                            series.Remove(ls);
+                            label1.Text = "E"+ ls[0];
+                        }
+                           
+                        return;
+                    }
+                    rand -= p;
+                }
+            }
+            limit = limit + 2;
+            calcular_siguiente_serie();
+        }
+
+
+        public void reset()
+        {
+            series.Add(new List<int>() { 36, 199419, 0 });
+            series.Add(new List<int>() { 28, 157841, 0 });
+            series.Add(new List<int>() { 27, 142412, 0 });
+            series.Add(new List<int>() { 29, 91381, 0 });
+            series.Add(new List<int>() { 37, 90968, 0 });
+            series.Add(new List<int>() { 30, 90321, 0 });
+            series.Add(new List<int>() { 26, 80842, 0 });
+            series.Add(new List<int>() { 35, 75497, 0 });
+            series.Add(new List<int>() { 32, 74867, 0 });
+            series.Add(new List<int>() { 34, 55090, 0 });
+            series.Add(new List<int>() { 31, 32089, 0 });
+            series.Add(new List<int>() { 33, 12105, 0 });
+            limit = 2;
+            duracion_mayor_absoluto = 0;
+            series_draw.Clear();
+            foreach (List<int> ls in series)
+            {
+                series_draw.Add(new List<int>() { ls[0],0 });
+                if (ls[1] > duracion_mayor_absoluto)
+                    duracion_mayor_absoluto = ls[1];
+            }
+        }
+        List<List<int>> series_draw = new List<List<int>>();
+        float duracion_mayor_absoluto;
+        public void draww()
+        {
+            {
+                //Pelicula
+                Bitmap bm = new Bitmap(picGraph.ClientSize.Width, picGraph.ClientSize.Height);
+                using (Graphics gr = Graphics.FromImage(bm))
+                {
+                    gr.SmoothingMode = SmoothingMode.AntiAlias;
+                    using (Pen pen = new Pen(Color.Red, 0))
+                    {
+                        using (Font font = new Font("Arial", 8))
+                        {
+                            pen.Color = Color.Black;
+                            var w = this.Width / tam;
+
+                            for (var i = 0; i < tam; i += 1)
+                            {
+                                var h = results[i] * 10;
+                                if (h != 0)
+                                {
+                                    id_s = 0;
+                                    gr.DrawRectangle(pen, i * 7, 0, 2, float.Parse(h + ""));
+                                }
+
+                            }
+                        } // Font
+                    } // Pen
+                    picGraph.Image = bm;
+                }
+            }
+            {
+                //serie
+                Bitmap bm = new Bitmap(picGraph.ClientSize.Width, picGraph.ClientSize.Height);
+                using (Graphics gr = Graphics.FromImage(bm))
+                {
+                    gr.SmoothingMode = SmoothingMode.AntiAlias;
+                    using (Pen pen = new Pen(Color.Red, 0))
+                    {
+                        using (Font font = new Font("Arial", 8))
+                        {
+                            pen.Color = Color.Black;
+                            var w = this.Width / tam;
+
+                            for (var i = 0; i < series.Count; i += 1)
+                                foreach (List<int> ls in series_draw)
+                                {
+                                    if (ls[0] == series[i][0]) {
+                                        ls[1] = series[i][2];
+                                    }
+                                }
+
+                            for (var i = 0; i < series_draw.Count; i += 1)
+                            {
+                                List<int> ls = series_draw[i];
+                                var h = ls[1] * 10;
+                                if (h != 0)
+                                    gr.DrawRectangle(pen, i * 7, 0, 2, float.Parse(h + ""));
+                            }
+
+                        } // Font
+                    } // Pen
+                    picGraph2.Image = bm;
+                }
+            }
+        }
+
+
+    }
+}
